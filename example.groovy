37141fb9e3a3d5c1561708fc8e8ea6b2e27e9d6a
sessiondevops <76641683+sessiondevops@users.noreply.github.com>
2021-04-01 01:00:55 +0530

    Update example.groovy

--- example.groovy
+++ example.groovy
@@ -1,4 +1,4 @@
-@Library(['github.com/shared-library']) _
+//@Library(['github.com/shared-library']) _
 
 def exampleMethod() {
     def file = new File ("http://13.52.98.219:8081/repository/maven_nikhil/com/marsh/et2/0.0.3-SNAPSHOT/maven-metadata.xml")
