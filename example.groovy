53bc5b401cb4474a611b9cfee826b0947bf0bf0c
sessiondevops <76641683+sessiondevops@users.noreply.github.com>
2021-04-01 00:23:01 +0530

    Create example.groovy

---
+++ example.groovy
@@ -0,0 +1,7 @@
+def exampleMethod() {
+    def file = new File 'http://13.52.98.219:8081/repository/maven_nikhil/com/marsh/et2/0.0.3-SNAPSHOT/maven-metadata.xml' 
+def xml = new XmlParser.parseText(file)
+println xml.@groupId
+}
+
+return this
