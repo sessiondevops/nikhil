5bd628bfce0b0cd95ee3b921b69b43dc01c657ab
sessiondevops <76641683+sessiondevops@users.noreply.github.com>
2021-04-01 00:46:02 +0530

    Update example.groovy

--- example.groovy
+++ example.groovy
@@ -1,3 +1,5 @@
+@Library(['github.com/shared-library']) _
+
 def exampleMethod() {
     def file = new File 'http://13.52.98.219:8081/repository/maven_nikhil/com/marsh/et2/0.0.3-SNAPSHOT/maven-metadata.xml' 
 def xml = new XmlParser.parseText(file)
